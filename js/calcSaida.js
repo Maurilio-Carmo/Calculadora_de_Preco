d1239a00c7d4ce0984a7b30ab927cdc4b19f1fc1
Maurilio-Carmo <mauriliodocarmo.job@gmail.com>
2025-12-06 17:56:37 -0300

    adiciona cálculos para CBS, IBSUF e IBSMun na função de processamento e atualiza a visualização dos resultados

--- js/calcSaida.js
+++ js/calcSaida.js
@@ -9,3 +9,15 @@ export function calcPisCofinsVenda(precoVenda, percVendaPisCofins, vICMSVenda) {
 export function calcICMSVenda(precoVenda, percVendaICMS, percReducaoICMSSaida) {
     return precoVenda * (percent(percVendaICMS) * (1 - percent(percReducaoICMSSaida)));
 }
+
+export function calcCBSVenda(precoVenda, percCBS, percReducaoCBS) {
+    return precoVenda * (percent(percCBS) * (1 - percent(percReducaoCBS)));
+}
+
+export function calcIBSUFVenda(precoVenda, percIBSUF, percReducaoIBSUF) {
+    return precoVenda * (percent(percIBSUF) * (1 - percent(percReducaoIBSUF)));
+}
+
+export function calcIBSMunVenda(precoVenda, percIBSMun) {
+    return precoVenda * percent(percIBSMun);
+}
